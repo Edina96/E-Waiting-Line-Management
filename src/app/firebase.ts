b5e147565f5337e3c5515e44b692e20f499c36ab
itsgabriell <gabrielp1chang@gmail.com>
2021-04-11 00:06:02 +0800

    Updated

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,9 +1,7 @@
+import { config } from "rxjs"
 
-// The list of file replacements can be found in `angular.json`.
-
-export const environment = {
-
-  production: false,
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyC-UufpSmQFWkKFytq3R8VF8w5buskgfyU",
   authDomain: "e-waiting-line-management.firebaseapp.com",
   databaseURL: "https://e-waiting-line-management-default-rtdb.firebaseio.com",
@@ -12,7 +10,6 @@ export const environment = {
   messagingSenderId: "577696203824",
   appId: "1:577696203824:web:ac8bfb0f2b8e207d4c99f5",
   measurementId: "G-1NCDZM2XQ7"
-}
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`*/
+  }
+
+  export default config
--- src/app/signup2/signup2.page.ts
+++ src/app/signup2/signup2.page.ts
@@ -1,41 +1,32 @@
-import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-
-interface SignUpModel {
-  email: string;
-  password: string;
-}
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-signup2',
   templateUrl: './signup2.page.html',
   styleUrls: ['./signup2.page.scss'],
 })
-export class Signup2Page implements OnInit {
+export class Signup2Page {
+  username: string="";
+  password: string="";
 
-  public signupForm = {} as SignUpModel;
-
-  constructor(public navCtrl: NavController) { }
-
+  constructor(public auth: AngularFireAuth,public navCtrl: NavController) {
+  }
   ngOnInit() {
   }
 
-  signup() {
-    console.log(this.signupForm);
-    this.navCtrl.navigateForward('signup2');
-  }
+  async register(){
+    const {username, password} = this
+    try{
+      const res = await this.auth.createUserWithEmailAndPassword(username,password)
+      console.log(res)
+      this.navCtrl.navigateForward('login');
+    }
+    catch(error){
+      console.dir(error)
+    }
 
-  login() {
-    console.log("login");
-    this.navCtrl.navigateForward('login');
-  }
-
-  continue() {
-    this.navCtrl.navigateForward('login');
-  }
-
-  back() {
-    this.navCtrl.navigateBack('signup');
-  }
-
+}
 }
