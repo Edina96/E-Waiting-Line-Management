0388222b0df18d11dfd7e41a8df3aea1b766d48e
Edina Phang <59107213+Edina96@users.noreply.github.com>
2021-04-12 20:57:39 +0800

    Updated authentication
    updated authentication

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { AngularFireAuth } from '@angular/fire/auth';
-import {Router} from '@angular/router'
+import { Router } from '@angular/router'
+import { first } from 'rxjs/operators';
 import firebase from 'firebase/app';
 
 @Component({
@@ -10,34 +11,32 @@ import firebase from 'firebase/app';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  username: string="";
-  password: string="";
+  username: string = "";
+  password: string = "";
   logindeterminer: boolean = false;
 
-  constructor(public auth: AngularFireAuth,public navCtrl: NavController,public router:Router) {
+  constructor(public auth: AngularFireAuth, public navCtrl: NavController, public router: Router) {
   }
   ngOnInit() {
+  }
 
+  isLoggedIn() {
+    return this.auth.authState.pipe(first()).toPromise();
   }
 
   //Login Function
   async login() {
-    const { username, password} = this;
-    try{
-      const res = this.auth.signInWithEmailAndPassword(username,password);
-      this.router.navigate(['shopSelection']);
-    }catch(err){
-      console.dir(err)
-      //If error displays user not found then
-      // if(err=="auth/user-not-found"){
-      //   console.log("User not found")
-      
-      // }
-    }
+    const { username, password } = this;
+
+    this.auth.signInWithEmailAndPassword(username, password).then(
+      async () => {
+        const user = await this.isLoggedIn();
+        if (user)
+          this.router.navigate(['shopSelection']);
+      },
+      (err) => console.log(err.message)
+    ).catch(err => alert(err.message));
   }
-  
-
-
 
   signup() {
     console.log("sign up");
